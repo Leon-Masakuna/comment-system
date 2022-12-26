8a0a258560ee9b063dd61f24a2bbcabafb945bf7
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-12-26 18:30:20 +0100

    feat : single comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-const Comment = ({ comment, replies }) => {
+const Comment = ({ comment, replies, currentUserId }) => {
+  const fiveMinutes = 300000;
+  const timePassed = new Date() - new Date(comment.createdAt) > fiveMinutes;
+  const canReply = Boolean(currentUserId);
+  const canEdit = currentUserId === comment.userId && !timePassed;
+  const canDelete = currentUserId === comment.userId && !timePassed;
   return (
     <div className="comment">
       <div className="comment-image-container">
@@ -12,6 +17,11 @@ const Comment = ({ comment, replies }) => {
           <div>{comment.createdAt}</div>
         </div>
         <div className="comment-text">{comment.body}</div>
+        <div className="comment-actions">
+          {canReply && <div className="comment-action">Reply</div>}
+          {canEdit && <div className="comment-action">Edit</div>}
+          {canDelete && <div className="comment-action">Delete</div>}
+        </div>
         {replies.length > 0 && (
           <div className="replies">
             {replies.map((reply) => (
