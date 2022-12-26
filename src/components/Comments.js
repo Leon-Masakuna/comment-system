98c47fed3b22b19c05df10e2ca61109fa4f31a09
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-12-26 16:11:54 +0100

    feat : all comment

---
+++ src/components/Comments.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { getComments as getCommentsApi } from "../data/api";
+import Comment from "./Comment";
+
+const Comments = ({ currentUserId }) => {
+  const [backendComments, setBackendComments] = useState([]);
+  const rootComments = backendComments.filter(
+    (backendComment) => backendComment.parentId === null
+  );
+
+  const getReplies = (commentId) => {
+    return backendComments
+      .filter((backendComment) => backendComment.parentId === commentId)
+      .sort(
+        (a, b) =>
+          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+      );
+  };
+
+  useEffect(() => {
+    getCommentsApi().then((data) => {
+      setBackendComments(data);
+    });
+  }, []);
+  return (
+    <div className="comments">
+      <h3 className="comments-title">Comments</h3>
+      <div className="comments-container">
+        {rootComments.map((rootComment) => (
+          <Comment
+            key={rootComment.id}
+            comment={rootComment}
+            replies={getReplies(rootComment.id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
