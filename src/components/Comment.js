c6a9a79064ba25cd46b5fe3812e81d9c8d05c362
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-12-27 16:27:52 +0100

    feat : single comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,15 +1,40 @@
 import React from "react";
+import CommentForm from "./CommentForm";
+import image from "../assets/user-icon.png";
+import "../index.css";
 
-const Comment = ({ comment, replies, currentUserId }) => {
+const Comment = ({
+  comment,
+  replies,
+  currentUserId,
+  addComment,
+  deleteComment,
+  activeComment,
+  setActiveComment,
+  parentId = null,
+}) => {
   const fiveMinutes = 300000;
   const timePassed = new Date() - new Date(comment.createdAt) > fiveMinutes;
   const canReply = Boolean(currentUserId);
   const canEdit = currentUserId === comment.userId && !timePassed;
   const canDelete = currentUserId === comment.userId && !timePassed;
+  /* const createdAt = new Date(comment.createdAt).toLocaleDateString(); */
+  const isReplying =
+    activeComment &&
+    activeComment.type === "replying" &&
+    activeComment.id === comment.id;
+
+  /* const isEditing =
+    activeComment &&
+    activeComment.type === "editing" &&
+    activeComment.id === comment.id; */
+
+  const replyId = parentId ? parentId : comment.id;
+
   return (
     <div className="comment">
       <div className="comment-image-container">
-        <img src="../assets/user-icon.png" alt="user" />
+        <img className="profile" src={image} alt="user" />
       </div>
       <div className="comment-right-part">
         <div className="comment-content">
@@ -18,14 +43,55 @@ const Comment = ({ comment, replies, currentUserId }) => {
         </div>
         <div className="comment-text">{comment.body}</div>
         <div className="comment-actions">
-          {canReply && <div className="comment-action">Reply</div>}
-          {canEdit && <div className="comment-action">Edit</div>}
-          {canDelete && <div className="comment-action">Delete</div>}
+          {canReply && (
+            <div
+              className="comment-action"
+              onClick={() =>
+                setActiveComment({ id: comment.id, type: "replying" })
+              }
+            >
+              Reply
+            </div>
+          )}
+          {canEdit && (
+            <div
+              className="comment-action"
+              onClick={() =>
+                setActiveComment({ id: comment.id, type: "editing" })
+              }
+            >
+              Edit
+            </div>
+          )}
+          {canDelete && (
+            <div
+              className="comment-action"
+              onClick={() => deleteComment(comment.id)}
+            >
+              Delete
+            </div>
+          )}
         </div>
+        {isReplying && (
+          <CommentForm
+            submitLabel="Reply"
+            handleSubmit={(text) => addComment(text, replyId)}
+          />
+        )}
         {replies.length > 0 && (
           <div className="replies">
             {replies.map((reply) => (
-              <Comment comment={reply} key={reply.id} replies={[]} />
+              <Comment
+                comment={reply}
+                key={reply.id}
+                replies={[]}
+                currentUserId={currentUserId}
+                deleteComment={deleteComment}
+                addComment={addComment}
+                parentId={comment.id}
+                activeComment={activeComment}
+                setActiveComment={setActiveComment}
+              />
             ))}
           </div>
         )}
